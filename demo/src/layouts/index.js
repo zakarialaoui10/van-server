12b3f0860f18641bad89dd9df810a41fbed2e860
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-26 14:27:38 +0100

    fix style

--- demo/src/layouts/index.js
+++ demo/src/layouts/index.js
@@ -1,5 +1,5 @@
 import van from "vanjs-core";
-// import "@styles/app.css"
+import "@styles/app.css"
 const {aside, main, div, nav, a} = van.tags;
 export default function Layout(Content){
     return div(
@@ -7,10 +7,10 @@ export default function Layout(Content){
         aside(
             nav(
                 { class : "navigation"},
-                a({href : "/me"}, "Home"),
-                a({href : "/me"}, "Get Started"),
-                a({href : "/me"}, "About"),
-                a({href : "/repos/owner/zakarialaoui10/repo/van-mdx"}, "Dynamic Routes")
+                a({href : "/"}, "Home"),
+                a({href : "/"}, "Features"),
+                a({href : "/repos/owner/zakarialaoui10/repo/van-mdx"}, "Dynamic Routes"),
+                a({href : "/about"}, "About"),
             )
         ),
         main(
--- demo/src/pages/repos/[owner]/[repo]/index.js
+++ demo/src/pages/repos/[owner]/[repo]/index.js
@@ -1,7 +1,7 @@
 import van from "vanjs-core";
 import Layout from "@layouts";
 import RepoCard from "@components/RepoCard.js";
-const {div, h1, p, a} = van.tags;
+const {div, nav, h2, p, a} = van.tags;
 import "@styles/app.css"
 import DynamicRoutes from "@contents/dynamic-routes.mdx"
 
@@ -11,8 +11,17 @@ const App = async({owner = "zakarialaoui10", repo = "van-mdx"} = {}) =>{
     const {stargazers_count, description, forks} = await resp.json()
     return Layout(
         div(
+            {style : "padding = 10px;"},
             DynamicRoutes(),
-            RepoCard(repo, description, stargazers_count, forks)
+            RepoCard(repo, description, stargazers_count, forks),
+            h2("Test Other Links"),
+            nav(
+                {class : "navigation"},
+                a({ href : "/repos/owner/vanjs-org/repo/van"}, "/repos/owner/vanjs-org/repo/van"),
+                a({ href : "/repos/owner/thednp/repo/vanjs-lucide"}, "/repos/owner/thednp/repo/vanjs-lucide"),
+                a({ href : "/repos/owner/zakarialaoui10/repo/van-mdx"}, "/repos/owner/zakarialaoui10/repo/van-mdx"),
+                a({ href : "/repos/owner/zakarialaoui10/repo/van-server"}, "/repos/owner/zakarialaoui10/repo/van-server")
+            )
         )
     )
 }
